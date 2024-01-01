85a756453097ece16f1983e371a3b705640f6690
Abdur Rakib <47861896+AbRakib@users.noreply.github.com>
2024-01-01 11:37:54 +0600

    Product filter by category

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -44,10 +45,17 @@ class ShopController extends Controller {
         }
         $brands = Brand::orderBy("name", "ASC")->get();
         $q_brands = $request->query("brands");
+        $categories = Category::orderBy("name", "ASC")->get();
+        $q_categories = $request->query("categories");
         $products = Product::where(function($query) use($q_brands) {
             $query->whereIn('brand_id', explode(',', $q_brands))->orWhereRaw("'".$q_brands."'=''");
-        })->orderBy('created_at', 'DESC')->orderBy($o_column, $o_order)->paginate($size);
-        return view('shop', compact('products', 'page', 'size', 'order', 'brands', 'q_brands'));
+        })
+        ->where(function($query) use($q_categories) {
+            $query->whereIn('category_id', explode(',', $q_categories))->orWhereRaw("'".$q_categories."'=''");
+        })
+        ->orderBy('created_at', 'DESC')->orderBy($o_column, $o_order)->paginate($size);
+
+        return view('shop', compact('products', 'page', 'size', 'order', 'brands', 'q_brands', 'categories', 'q_categories'));
     }
 
     public function productDetails($slug) {
